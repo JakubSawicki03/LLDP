18a53e555532d4229b9bee2ae6bd33eaf6a7dd7e
Jakub Sawicki <142387174+JakubSawicki03@users.noreply.github.com>
2023-09-13 08:57:13 +0200

    Update LLDP.ps1

--- LLDP.ps1
+++ LLDP.ps1
@@ -1,14 +1,18 @@
+Set-ExecutionPolicy Bypass;
+
 #   Checking for installed module. If Installed script is going to 
 #   check the connection to the switch. 
-#   If not it will install module, then check the connection.
+#   If not it will install the module, then check the connection.
 
 if (Get-Module PSDiscoveryProtocol -ListAvailable) {            
     $Packet = Invoke-DiscoveryProtocolCapture -Type LLDP;       
-    Get-DiscoveryProtocolData -Packet $Packet;
+    $Results = Get-DiscoveryProtocolData -Packet $Packet;
+    Write-Output $Results | Out-String;
 }
 
 else {
     Install-Module -name PSDiscoveryProtocol -force;
     $Packet = Invoke-DiscoveryProtocolCapture -Type LLDP;
-    Get-DiscoveryProtocolData -Packet $Packet;
-};
+    $Results = Get-DiscoveryProtocolData -Packet $Packet;
+    Write-Output $Results | Out-String;
+}; 
